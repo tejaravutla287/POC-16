91bbe087f7d48a7eafea606828aad7e5af849553
tejaravutla287 <tejaravutla287@gmail.com>
2026-07-16 15:35:53 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,5 +1,5 @@
-output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
+output "aks_name" {
+  value = azurerm_kubernetes_cluster.aks.name
 }
 
 output "acr_name" {
@@ -10,10 +10,10 @@ output "acr_login_server" {
   value = azurerm_container_registry.acr.login_server
 }
 
-output "container_app_environment_id" {
-  value = azurerm_container_app_environment.env.id
+output "cosmos_name" {
+  value = azurerm_cosmosdb_account.cosmos.name
 }
 
-output "log_analytics_workspace_id" {
-  value = azurerm_log_analytics_workspace.law.id
+output "resource_group" {
+  value = azurerm_resource_group.rg.name
 }
