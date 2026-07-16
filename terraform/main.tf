732dff9de1860a8d4ecb0c048f634dc9a8882a0a
tejaravutla287 <tejaravutla287@gmail.com>
2026-07-16 15:35:36 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>4.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-  subscription_id = var.subscription_id
-}
-
 resource "azurerm_resource_group" "rg" {
   name     = "poc16-rg"
   location = "Central India"
@@ -25,16 +11,37 @@ resource "azurerm_container_registry" "acr" {
   admin_enabled       = true
 }
 
-resource "azurerm_log_analytics_workspace" "law" {
-  name                = "poc16-law"
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "poc16-aks"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  sku                 = "PerGB2018"
+  dns_prefix          = "poc16"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_B2s"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
 }
 
-resource "azurerm_container_app_environment" "env" {
-  name                       = "poc16-env"
-  location                   = azurerm_resource_group.rg.location
-  resource_group_name        = azurerm_resource_group.rg.name
-  log_analytics_workspace_id = azurerm_log_analytics_workspace.law.id
+resource "azurerm_cosmosdb_account" "cosmos" {
+
+  name                = "poc16cosmosdb"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  offer_type = "Standard"
+
+  consistency_policy {
+    consistency_level = "Session"
+  }
+
+  geo_location {
+    location          = azurerm_resource_group.rg.location
+    failover_priority = 0
+  }
 }
