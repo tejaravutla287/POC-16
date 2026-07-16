7d951e513eec801f4edf14121f72c4f20f74d4cd
tejaravutla287 <tejaravutla287@gmail.com>
2026-07-16 15:18:08 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "poc16-rg"
+  location = "Central India"
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = "poc16acr12345"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+resource "azurerm_log_analytics_workspace" "law" {
+  name                = "poc16-law"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "PerGB2018"
+}
+
+resource "azurerm_container_app_environment" "env" {
+  name                       = "poc16-env"
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.law.id
+}
